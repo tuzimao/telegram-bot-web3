1be312438d99589243968ab0c4d6e95463b7099d
tuzimao <zliu1@ualberta.ca>
2023-10-06 12:21:53 -0700

    installed ether to front socket bug fixed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -18,6 +18,7 @@ import { useAccount } from "wagmi";
 import { sendAddressToServer } from "./getWalletAddressAPI";
 import LotteryManagerABI from "./LotteryManagerABI.json";
 import io from "socket.io-client";
+import { Wallet, ethers } from "ethers";
 interface TicketRequest {
   lotteryId: string;
   numberOfTickets: string;
@@ -100,6 +101,46 @@ const WalletStatus = () => {
   return <div>Connected Wallet: {walletAddress}</div>;
 };
 
+const buyTickets = async (
+  lotteryId: number,
+  numberOfTickets: number,
+  userAddress: string
+): Promise<void> => {
+  // 确保你的 web3 provider 可用
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const signer = provider.getSigner();
+
+    // 创建一个新的合约实例
+    const lotteryContract = new ethers.Contract(
+      contractAddress,
+      LotteryManagerABI,
+      signer
+    ) as ethers.Contract;
+
+    // 计算发送的金额（根据你的合约逻辑，这可能需要更改）
+    const bignumberOfTickets = BigInt(numberOfTickets);
+    const TICKET_PRICE = ethers.parseEther("0.0001"); // 假设票的价格是 0.0001 ETH
+    const totalValue = TICKET_PRICE * bignumberOfTickets;
+
+    try {
+      // 调用合约的 buyTickets 函数
+      const tx = await lotteryContract.buyTickets(lotteryId, numberOfTickets, {
+        value: totalValue,
+        from: userAddress,
+      });
+
+      // 等待交易被确认
+      const receipt = await tx.wait();
+      console.log("Transaction receipt:", receipt);
+    } catch (error) {
+      console.error("Error purchasing tickets:", error);
+    }
+  } else {
+    console.error("Ethereum provider is not available");
+  }
+};
+
 const App = () => {
   const [ticketRequest, setTicketRequest] = useState<TicketRequest | null>(
     null
@@ -116,7 +157,17 @@ const App = () => {
       console.log("Received buyTicketRequest:", data);
       setTicketRequest(data);
     });
-
+    const { address } = useAccount();
+    if (address) {
+      // 调用 buyTickets 函数执行购买
+      await buyTickets(
+        parseInt(data.lotteryId),
+        parseInt(data.numberOfTickets),
+        address
+      );
+    } else {
+      console.error("No connected wallet address found.");
+    }
     // Clean up the socket connection when the component is unmounted
     return () => {
       socket.disconnect();
